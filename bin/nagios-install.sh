ad8b7ddaa22076fc8d99860993c2d7040bfe5789
Novan@480s <55051643+Novanz@users.noreply.github.com>
2024-11-26 17:09:36 +0100

    feat(nagios): Check if root

--- bin/nagios-install.sh
+++ bin/nagios-install.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
 # Automatic installation script for latest nagios-core + plugins from git
+# 
+#
+#
+#
+# check if running as root
+if [ $EUID -ne 0 ]; then
+	echo "This script is not being run as root."
+	echo "Please run it as root user by using either sudo or su command."
+	exit 1
+fi
+VERSION=$(curl --silent -qI https://github.com/NagiosEnterprises/nagioscore/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}');
+echo $VERSION
+mkdir -p test
+cd test
+wget https://github.com/NagiosEnterprises/nagioscore/releases/tag/$VERSION/$VERSION.tar.gz
